0a7c9471d9f3da8d62cf4cb971c2d3a09ada60b7
Johan Book <johanbook@hotmail.com>
2018-03-08 13:28:51 +0100

    Update Differentiable.java

--- src/solver/Differentiable.java
+++ src/solver/Differentiable.java
@@ -2,6 +2,7 @@ package solver;
 
 import settings.Settings;
 
-public interface Differentiable {
+public interface Differentiable 
+{
 	public double[] differentiate(double[] y, double[] x, double t, final Settings settings);
 }
